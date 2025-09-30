b2ffc42a20d8ee2f54b841e6b198fe27a658e6a7
villelyytikainen <corpsefin@gmail.com>
2025-09-30 03:00:51 +0300

    event tests

--- client/client.ts
+++ client/client.ts
@@ -1,6 +1,21 @@
+import { log } from "node:console";
 import { places } from "./data/places";
+import { isNumberObject } from "node:util/types";
 
 const Delay = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
+const waitForCar = async (hash: number) => {
+    RequestModel(hash);
+
+    AddTextEntry("CUSTOM_ENTRY", "come on boyyy, lets wait for the car boyyy");
+    BeginTextCommandDisplayHelp("CUSTOM_ENTRY");
+    EndTextCommandDisplayHelp(0, false, true, -1);
+    let count = 0;
+    while (!HasModelLoaded(hash)) {
+        await Delay(100);
+        console.log(count++);
+    }
+};
 
 RegisterCommand(
     "c",
@@ -9,9 +24,7 @@ RegisterCommand(
         const modelHash = GetHashKey(model);
 
         if (!IsModelAVehicle(modelHash)) return;
-        RequestModel(modelHash);
-        while (!HasModelLoaded) await Delay(100);
-
+        await waitForCar(modelHash);
         const [x, y, z] = GetEntityCoords(PlayerPedId(), true);
         const h = GetEntityHeading(PlayerPedId());
         const veh = CreateVehicle(modelHash, x, y, z, h, true, true);
@@ -24,76 +37,101 @@ RegisterCommand(
 );
 
 RegisterCommand(
+    "killme",
+    () => {
+        // PlayEntityAnim(PlayerPedId(),"idle","mp_sleep", 2,false,false,true, 2,0);
+        ClearPedTasks(PlayerPedId());
+        TaskPlayAnim(PlayerPedId(), "mp_suicide", "pill", 8.0, 8.0, -1, 128, 1, false, false, false);
+        // SetEntityHealth(PlayerPedId(), 0);
+    },
+    false
+);
+
+RegisterCommand(
     "tp",
     async (source: number, args: string[], rawCommand: string) => {
-        let p = null;
-        if ((p = places.find((place) => place.name === args[0]))) {
+        args[0] = args[0].split(" ").join("");
+
+        const p = places.find((place) => place.name === args[0]) ?? null;
+        if (p) {
             StartPlayerTeleport(PlayerId(), p.x, p.y, p.z, 1, true, true, true);
         } else {
-            AddTextEntry("CUSTOM_ENTRY", "No position found! Check input");
-            BeginTextCommandDisplayHelp("CUSTOM_ENTRY");
-            EndTextCommandDisplayHelp(0, false, true, -1);
+
+            const [x = 0, y = 0, z = 0] = (args[0] || "").split(",");
+            const pos = { x, y, z };
+
+            // AddTextEntry("CUSTOM_ENTRY", `Position is now: ${JSON.stringify(pos)}`);
+            // BeginTextCommandDisplayHelp("CUSTOM_ENTRY");
+            // EndTextCommandDisplayHelp(0, false, true, -1);
+
+            console.log(pos);
+
+            StartPlayerTeleport(PlayerId(), Number(pos.x), Number(pos.y), Number(pos.z), 1, true, true, true);
         }
     },
     false
 );
 
-// on("CEventOpenDoor", (name:string) => {
-//     AddTextEntry("CUSTOM_ENTRY", "Some door opened nearby");
-//     BeginTextCommandDisplayHelp("CUSTOM_ENTRY");
-//     EndTextCommandDisplayHelp(0, false, true, -1);
-//     whoOpened(name);
-// });
-
-on("CEventOpenDoor", (entity: number, object_hash: number, object_name: string) => {
-    AddTextEntry("CUSTOM_ENTRY", "Some door opened nearby");
+on("CEventVehicleDamage", () => {
+    AddTextEntry("CUSTOM_ENTRY", "vehicle damage");
     BeginTextCommandDisplayHelp("CUSTOM_ENTRY");
     EndTextCommandDisplayHelp(0, false, true, -1);
-
-    const args = {
-        entity,
-        object_hash,
-        object_name,
-    };
-
-    test(args);
 });
 
 const whoOpened = (name: string) => {
     const pName = GetPlayerName(PlayerId());
+    console.log(pName);
     emitNet("server:Greet", name);
 };
 
-const test = (...args: any[]) => {
-    emitNet("server:Test", args);
-};
-
-on("onClientGameTypeStart", () => {
-    exports.spawnmanager.setAutoSpawnCallback(() => {
-        exports.spawnmanager.spawnPlayer(
-            {
-                x: 686.245,
-                y: 577.95,
-                z: 130.461,
-                model: "a_m_m_skater_01",
-            },
-            () => {
-                emit("chat:addMessage", {
-                    args: ["Hi, there!"],
-                });
-            }
-        );
+on("CEventOpenDoor", (name: string) => {
+    AddTextEntry("CUSTOM_ENTRY", "Some door opened nearby");
+    BeginTextCommandDisplayHelp("CUSTOM_ENTRY");
+    EndTextCommandDisplayHelp(0, false, true, -1);
+    whoOpened(name);
+    emit("chat:addMessage", {
+        args: ["hey, my door fucker"],
     });
-
-    exports.spawnmanager.setAutoSpawn(true);
-    exports.spawnmanager.forceRespawn();
 });
+
+on("pedranover", () => {
+    AddTextEntry("CUSTOM_ENTRY", "pedranover");
+    BeginTextCommandDisplayHelp("CUSTOM_ENTRY");
+    EndTextCommandDisplayHelp(0, false, true, -1);
+});
+
+on("CEventRanOverPed", (...args: any[]) => {
+    console.log(args);
+
+    emit("pedranover");
+});
+
+RegisterCommand(
+    "st",
+    async (source: number, args: string[], rawCommand: string) => {
+        SetGpsActive(true);
+        SetPlayerMaxStamina(PlayerId(), 1000);
+        0x733a643b5b0c53c1;
+    },
+    false
+);
+
+RegisterCommand(
+    "want",
+    async (source: number, args: string[], rawCommand: string) => {
+        GetPlayerFakeWantedLevel(PlayerId());
+        console.log(args);
+        const level: number = args.length > 0 ? Number(args[0]) : 0;
+        SetPlayerWantedLevel(PlayerId(), level, false);
+    },
+    false
+);
 
 setImmediate(() => {
     emit("chat:addSuggestion", "/greet", "Run this to say yo to other player", [
         {
             name: "name",
-            help: "name of the player you wanna yoyo",
+            help: "name of the player you wanna oi at",
         },
     ]);
     emit("chat:addSuggestion", "/tp", "Teleport to a wanted location", [
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'An awesome dude'
+description 'An awesome, but short, description'
+version '1.0.0'
+-- resource_type 'gametype' { name = 'My awesome game type!' }
+
 client_scripts {
 	'build/client/client.js'
 }
